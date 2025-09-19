a70e0fe7f36dea6dece00dae55b88ad9c7586f92
roll-raihan <raihanahmednasim@gmail.com>
2025-09-19 16:40:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Countries from './countries/Countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>My Name Is Raihan</h1>
+        </div>
+    );
+};
+
+export default Countries;
